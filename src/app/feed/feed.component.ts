31316677b372cad0a10730dca2f6da96fb0d5abf
Abdelrahman Alkot <abdelrahman.alkot2182004@gmail.com>
2025-04-26 00:45:12 +0300

    Added Feed with dummy post, and override angular material theme

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Post } from '../app.model';
+import { dummyPosts } from './dummy-posts';
+import { PostComponent } from "./post/post.component";
 
 @Component({
   selector: 'app-feed',
-  imports: [],
+  imports: [PostComponent],
   templateUrl: './feed.component.html',
   styleUrl: './feed.component.scss'
 })
 export class FeedComponent {
-
+  posts=signal<Post[]>(dummyPosts);
 }
--- src/app/feed/post/post.component.ts
+++ src/app/feed/post/post.component.ts
@@ -1,19 +1,25 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { Post } from '../../app.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-post',
-  imports: [MatCardModule, MatButtonModule, MatIconModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, DatePipe],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
 })
 export class PostComponent {
   post=input.required<Post>();
+  isLikedByLoggedInUser=computed<boolean>(()=>(this.post().likes.find((like)=>like.userId===102)!==undefined));
 
   get imgUrl(){
     return this.post().postOwner.profilePicURL;
+  }
+
+  get altImg(){
+    return (this.post().postOwner.gender==='Male')? 'default-male-avatar' : 'default-female-avatar';
   }
 }
