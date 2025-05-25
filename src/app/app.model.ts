a94736e086e908e87f3a337ff109a1d3fe5c746f
Mostafa Ayman <mostafaayman14g@gmail.con>
2025-05-25 15:41:20 -0700

    Messages styling done

--- src/app/app.model.ts
+++ src/app/app.model.ts
@@ -7,3 +7,5 @@ export type Comment={id: number, text: string, user: User, dateCreated: Date};
 export type Like={userId: number, time: Date};
 
 export type Post={id: number, text_content: string, postOwner: User, datePosted: Date, attachedImagesURLs: string[], likes: Like[], comments: Comment[]};
+
+export type Message={id: number, text: string, sender: User,reciever:User, dateCreated: Date};
